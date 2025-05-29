12d14d6276a09ae98cd5883a136814a20d4913bc
hj <cody978@naver.com>
2025-05-29 14:08:01 +0900

    Update0529_

--- Web/settings.py
+++ Web/settings.py
@@ -55,9 +55,9 @@ def get_secret(setting, secrets=secrets):
 SECRET_KEY = get_secret("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
+DEBUG = False
 
-DEBUG = True
+#DEBUG = True
 
 
 
